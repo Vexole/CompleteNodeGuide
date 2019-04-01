3039136cc38ea3a73e4c4b83a6ccc217c9c6f477
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-01 16:51:58 +0545

    Request/Response

--- section-3-app.js
+++ section-3-app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
       console.log(req);
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>First Application</title></head>');
+      res.write('<body><p>This is the first application.</p></body>');
+      res.write('</html>');
+      res.end(); 
 }); 
 
 server.listen(3000);
