e628b283bfae6f0c48ed724692e102b351440d7c
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-01 17:01:25 +0545

    Simple Routing Request

--- section-3-app.js
+++ section-3-app.js
@@ -1,13 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-      console.log(req);
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html>');
-      res.write('<head><title>First Application</title></head>');
-      res.write('<body><p>This is the first application.</p></body>');
-      res.write('</html>');
-      res.end(); 
+    const url = req.url;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>First Application</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end(); 
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Application</title></head>');
+    res.write('<body><p>This is the first application.</p></body>');
+    res.write('</html>');
+    res.end(); 
 }); 
 
-server.listen(3000);
+server.listen(3000); 
