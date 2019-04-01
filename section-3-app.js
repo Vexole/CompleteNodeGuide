758b1c240b7de82e68e78fadb1ed2a1e42877c76
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-01 17:07:47 +0545

    Redirecting Requests

--- section-3-app.js
+++ section-3-app.js
@@ -1,14 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    if(url === '/') {
+    const method = req.method;
+
+    if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>First Application</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end(); 
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Test');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
