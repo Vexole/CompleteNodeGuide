527811322857b7f3b862a420ffd5f7eacbcb26d8
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-02 15:55:56 +0545

    Understanding Middleware

--- Section 5/app.js
+++ Section 5/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 app.use((req, res, next ) => {
     console.log("In the middleware");
+    res.send('<h1>Hello from Express</h1>');
 });
 
 const server = http.createServer(app);
