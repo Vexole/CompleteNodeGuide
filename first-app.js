cfec19a836ebbcef567b92d1eda422fbc00b7c3c
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-02 14:41:51 +0545

    Restructuring

--- first-app.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('firstFile.txt', 'Hello from Node.js');
--- section-3-app.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const routes = require('./section-3-routes');
-
-const server = http.createServer(routes); 
-
-server.listen(3000); 
