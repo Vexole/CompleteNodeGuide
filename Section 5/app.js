caca6ed63f4449f5fa764861fb34931b32ee6cd5
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-02 16:20:38 +0545

    Handling routes

--- Section 5/app.js
+++ Section 5/app.js
@@ -2,8 +2,12 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-app.use((req, res, next ) => {
-    console.log("In the middleware");
+
+app.use('/users', (req, res, next ) => {
+    res.send('<h1>Users</h1>');
+});
+
+app.use('/', (req, res, next ) => {
     res.send('<h1>Hello from Express</h1>');
 });
 
