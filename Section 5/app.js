2569e2d5cec928a92dfd88ca858b2eaf08952934
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-02 15:53:16 +0545

    Adding Middleware

--- Section 5/app.js
+++ Section 5/app.js
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next ) => {
+    console.log("In the middleware");
+});
 
 const server = http.createServer(app);
 
