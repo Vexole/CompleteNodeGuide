699a1564c8546eb109facc9c4b4faec8bab033a0
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-02 22:40:05 +0545

    Serving html pages

--- Section 5/Assignment 2/routes/shop.js
+++ Section 5/Assignment 2/routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<p>Home Page</p>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
