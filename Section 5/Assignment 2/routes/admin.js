17418494328e5e77813a73894181358e15296dbc
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-04-04 11:04:05 +0545

    Style changes and sharing data

--- Section 5/Assignment 2/routes/admin.js
+++ Section 5/Assignment 2/routes/admin.js
@@ -4,14 +4,17 @@ const path = require('path');
 const router = express.Router();
 const rootDir = require('../util/path');
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /> <button type="submit">Add Product</button></form>');
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });    
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
